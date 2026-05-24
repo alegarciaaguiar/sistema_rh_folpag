b745aa0ed5395740107b928a3378997c00b18aa6
Lucas Marinho da Mata Almeida <103390724+Marinhodev@users.noreply.github.com>
2026-05-24 14:18:31 -0300

    Fiz a implementação noa classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import model.Cargo;
+import model.Funcionario;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Funcionario> funcionario = new ArrayList<>();
+
+        System.out.println("=== CADASTRO FUNCIONARIO ===");
+
+        System.out.print("ID:");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Nome:");
+        String nome = sc.nextLine();
+        sc.nextLine();
+
+        System.out.print("CPF:");
+        String cpf = sc.nextLine();
+        sc.nextLine();
+
+        System.out.print("Cargo:");
+        String nomeCargo = sc.nextLine();
+
+        System.out.print("Salario Base:");
+        Double salario = sc.nextDouble();
+
+        Cargo cargo = new Cargo(1, nomeCargo, salario);
+
+        Funcionario funcionario = new Funcionario(id, nome, cpf, cargo, salario, 0, 0);
+
+        funcionario.add(funcionario);
+        System.out.println("\n FUNCIONARIO CADASTRADO!");
+
+
+        System.out.println("\n=== FUNCIONARIO ===");
+
+        for (Funcionario f : funcionarios) {
+            System.out.println(f.getNome());
+
+            System.out.println(f.getCargo().getNome());
+
         }
     }
+
+
+
+
+
+    }
+
+
+
+
+
 }
